2062ed420e21703aefa1715f14f3ef8bf1b822a3
Surya Teja Chavali <teja5832@users.noreply.github.com>
2017-11-02 00:57:01 -0500

    class_attrs.py
    Get class attributes

---
+++ class_attrs.py
@@ -0,0 +1,20 @@
+
+class cls(object):
+    
+    def attr_gen(self):
+        attrs = set(dir(self)) - set(dir(object))
+        for attr in attrs:
+            a = getattr(self, attr, None)
+            if a is not None and type(a) is not np.ndarray and not(inspect.isfunction(a) or inspect.ismethod(a) or inspect.isclass(a)):
+                yield attr.upper()
+                yield a
+    def printState(self):
+        nprint('Tree Attributes:', *self.attr_gen())
+        nprint('\n\n')
+        
+    def nprint(farg, *args):
+        print farg
+        for arg in args:
+          print arg
+
+
